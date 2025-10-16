5e56a86731278d4a214c5b048cac78f1de6825a9
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-10-16 16:29:45 +0300

    Добавил вкладочки

--- Interface/FormMain.Designer.cs
+++ Interface/FormMain.Designer.cs
@@ -36,7 +36,12 @@
 			ClassesToolStripMenuItem = new ToolStripMenuItem();
 			toolStripMenuItemAddClass = new ToolStripMenuItem();
 			toolStripMenuItemEditClass = new ToolStripMenuItem();
+			tabControlMain = new TabControl();
+			tabPageStudents = new TabPage();
+			tabPageClasses = new TabPage();
+			tabPagePlans = new TabPage();
 			menuStrip1.SuspendLayout();
+			tabControlMain.SuspendLayout();
 			SuspendLayout();
 			// 
 			// menuStrip1
@@ -58,7 +63,7 @@
 			// toolStripMenuItemClose
 			// 
 			toolStripMenuItemClose.Name = "toolStripMenuItemClose";
-			toolStripMenuItemClose.Size = new Size(180, 22);
+			toolStripMenuItemClose.Size = new Size(109, 22);
 			toolStripMenuItemClose.Text = "&Выйти";
 			toolStripMenuItemClose.Click += CloseButton_Click;
 			// 
@@ -72,14 +77,14 @@
 			// toolStripMenuItemAddStudent
 			// 
 			toolStripMenuItemAddStudent.Name = "toolStripMenuItemAddStudent";
-			toolStripMenuItemAddStudent.Size = new Size(180, 22);
+			toolStripMenuItemAddStudent.Size = new Size(154, 22);
 			toolStripMenuItemAddStudent.Text = "&Добавить";
 			toolStripMenuItemAddStudent.Click += StudentAddtoolStripMenuItem_Click;
 			// 
 			// toolStripMenuItemEditStudent
 			// 
 			toolStripMenuItemEditStudent.Name = "toolStripMenuItemEditStudent";
-			toolStripMenuItemEditStudent.Size = new Size(180, 22);
+			toolStripMenuItemEditStudent.Size = new Size(154, 22);
 			toolStripMenuItemEditStudent.Text = "&Редактировать";
 			toolStripMenuItemEditStudent.Click += StudentEditToolStripMenuItem_Click;
 			// 
@@ -93,28 +98,71 @@
 			// toolStripMenuItemAddClass
 			// 
 			toolStripMenuItemAddClass.Name = "toolStripMenuItemAddClass";
-			toolStripMenuItemAddClass.Size = new Size(180, 22);
+			toolStripMenuItemAddClass.Size = new Size(154, 22);
 			toolStripMenuItemAddClass.Text = "&Добавить";
 			toolStripMenuItemAddClass.Click += ClassAddStripMenuItem_Click;
 			// 
 			// toolStripMenuItemEditClass
 			// 
 			toolStripMenuItemEditClass.Name = "toolStripMenuItemEditClass";
-			toolStripMenuItemEditClass.Size = new Size(180, 22);
+			toolStripMenuItemEditClass.Size = new Size(154, 22);
 			toolStripMenuItemEditClass.Text = "&Редактировать";
 			toolStripMenuItemEditClass.Click += ClassEditStripMenuItem_Click;
+			// 
+			// tabControlMain
+			// 
+			tabControlMain.Controls.Add(tabPageStudents);
+			tabControlMain.Controls.Add(tabPageClasses);
+			tabControlMain.Controls.Add(tabPagePlans);
+			tabControlMain.Location = new Point(0, 27);
+			tabControlMain.Name = "tabControlMain";
+			tabControlMain.SelectedIndex = 0;
+			tabControlMain.Size = new Size(800, 422);
+			tabControlMain.TabIndex = 1;
+			// 
+			// tabPageStudents
+			// 
+			tabPageStudents.Location = new Point(4, 24);
+			tabPageStudents.Name = "tabPageStudents";
+			tabPageStudents.Padding = new Padding(3);
+			tabPageStudents.Size = new Size(792, 394);
+			tabPageStudents.TabIndex = 0;
+			tabPageStudents.Text = "Студенты";
+			tabPageStudents.UseVisualStyleBackColor = true;
+			// 
+			// tabPageClasses
+			// 
+			tabPageClasses.Location = new Point(4, 24);
+			tabPageClasses.Name = "tabPageClasses";
+			tabPageClasses.Padding = new Padding(3);
+			tabPageClasses.Size = new Size(792, 394);
+			tabPageClasses.TabIndex = 1;
+			tabPageClasses.Text = "Занятия";
+			tabPageClasses.UseVisualStyleBackColor = true;
+			// 
+			// tabPagePlans
+			// 
+			tabPagePlans.Location = new Point(4, 24);
+			tabPagePlans.Name = "tabPagePlans";
+			tabPagePlans.Padding = new Padding(3);
+			tabPagePlans.Size = new Size(792, 394);
+			tabPagePlans.TabIndex = 2;
+			tabPagePlans.Text = "Планы";
+			tabPagePlans.UseVisualStyleBackColor = true;
 			// 
 			// MainForm
 			// 
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
 			ClientSize = new Size(800, 450);
+			Controls.Add(tabControlMain);
 			Controls.Add(menuStrip1);
 			MainMenuStrip = menuStrip1;
 			Name = "MainForm";
 			Text = "Факультативы";
 			menuStrip1.ResumeLayout(false);
 			menuStrip1.PerformLayout();
+			tabControlMain.ResumeLayout(false);
 			ResumeLayout(false);
 			PerformLayout();
 		}
@@ -130,5 +178,9 @@
 		private ToolStripMenuItem toolStripMenuItemEditStudent;
 		private ToolStripMenuItem toolStripMenuItemAddClass;
 		private ToolStripMenuItem toolStripMenuItemEditClass;
+		private TabControl tabControlMain;
+		private TabPage tabPageStudents;
+		private TabPage tabPageClasses;
+		private TabPage tabPagePlans;
 	}
 }
