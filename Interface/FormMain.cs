04bfe8887bdf854d7fc3da5dec8c0106b189f659
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-11-01 16:28:49 +0300

    Изменил оформление, сократил короткие методы

--- Interface/FormMain.cs
+++ Interface/FormMain.cs
@@ -1,103 +1,86 @@
 using Electives;
 
-namespace Interface
+namespace Interface;
+
+/// <summary> Основная форма </summary>
+public partial class FormMain : Form
 {
-	/// <summary> Основная форма </summary>
-	public partial class FormMain : Form
+	/// <summary> Внутреннее поле со студентом </summary>
+	private Electives.Student _student = new();
+
+	/// <summary> Внутреннее поле с занятием </summary>
+	private Electives.Class _class = new();
+
+	public FormMain () => this.InitializeComponent();
+
+	/// <summary> Метод для закрытия приложения через пункт меню </summary>
+	private void CloseButton_Click (object sender, EventArgs e) => this.Close();
+
+	/// <summary>
+	/// Метод для вызова и обработки результата работы формы изменения студента
+	/// </summary>
+	/// <param name="student">Обрабатываемый студент</param>
+	private void AddOrEditStudent (Electives.Student student)
 	{
-		/// <summary> Внутреннее поле со студентом </summary>
-		private Electives.Student _student = new();
-
-		/// <summary> Внутреннее поле с занятием </summary>
-		private Electives.Class _class = new();
-
-		public FormMain ()
-		{
-			InitializeComponent();
+		var form = new FormStudent(student);
+		if (DialogResult.OK != form.ShowDialog()) {
+			return;
 		}
 
-		/// <summary> Метод для закрытия приложения через пункт меню </summary>
-		private void CloseButton_Click (object sender, EventArgs e)
-		{
-			Close();
+		if (form.Student == null) {
+			MessageBox.Show(
+				"StudentEditForm вернула null",
+				"Внутренняя ошибка"
+			);
+			return;
+		}
+		if (!form.Student.IsValid) {
+			MessageBox.Show("Неправильно указаны данные!");
+			return;
 		}
 
-		/// <summary>
-		/// Метод для вызова и обработки результата работы формы изменения студента
-		/// </summary>
-		/// <param name="student">Обрабатываемый студент</param>
-		private void AddOrEditStudent (Electives.Student student)
-		{
-			var form = new FormStudent(student);
-			if (DialogResult.OK != form.ShowDialog()) {
-				return;
-			}
-
-			if (form.Student == null) {
-				MessageBox.Show(
-					"StudentEditForm вернула null",
-					"Внутренняя ошибка"
-				);
-				return;
-			}
-			if (!form.Student.IsValid) {
-				MessageBox.Show("Неправильно указаны данные!");
-				return;
-			}
-
-			this._student = form.Student;
-		}
-
-		/// <summary> Обработчик поля создания нового студента </summary>
-		private void StudentAddtoolStripMenuItem_Click (object sender, EventArgs e)
-		{
-			//Вызов формы на чистом студенте
-			AddOrEditStudent(new Student());
-		}
-
-		/// <summary> Обработчик редактирования существующего студента </summary>
-		private void StudentEditToolStripMenuItem_Click (object sender, EventArgs e)
-		{
-			//Вызов формы на копии исходного студента
-			AddOrEditStudent(_student.Clone());
-		}
-
-		/// <summary>
-		/// Метод для вызова и обработки результата работы формы изменения занятия
-		/// </summary>
-		/// <param name="class">Обрабатываемое занятие</param>
-		private void AddOrEditClass(Electives.Class @class)
-		{			
-			var form = new FormClass(@class);
-			if (DialogResult.OK != form.ShowDialog()) {
-				return;
-			}
-
-			if (form.Class == null) {
-				MessageBox.Show(
-					"ClassEditForm вернула null",
-					"Внутренняя ошибка"
-				);
-				return;
-			}
-			if (!form.Class.IsValid) {
-				MessageBox.Show("Неправильно указаны данные!");
-				return;
-			}
-
-			this._class = form.Class;
-		}
-
-		/// <summary> Обработчик создания нового занятия </summary>
-		private void ClassAddStripMenuItem_Click (object sender, EventArgs e)
-		{
-			AddOrEditClass(new Class());
-		}
-
-		/// <summary> Обработчик редактирования существующего занятия </summary>
-		private void ClassEditStripMenuItem_Click (object sender, EventArgs e)
-		{
-			AddOrEditClass(_class.Clone());
-		}
+		this._student = form.Student;
 	}
+
+	/// <summary> Обработчик поля создания нового студента </summary>
+	private void StudentAddtoolStripMenuItem_Click (object sender, EventArgs e) 
+		=> this.AddOrEditStudent(new Student()); //Вызов формы на чистом студенте
+
+	/// <summary> Обработчик редактирования существующего студента </summary>
+	private void StudentEditToolStripMenuItem_Click (object sender, EventArgs e)
+		=> this.AddOrEditStudent(_student.Clone()); //Вызов формы на копии исходного студента
+	
+	/// <summary>
+	/// Метод для вызова и обработки результата работы формы изменения занятия
+	/// </summary>
+	/// <param name="class">Обрабатываемое занятие</param>
+	private void AddOrEditClass (Electives.Class @class)
+	{
+		var form = new FormClass(@class);
+		if (DialogResult.OK != form.ShowDialog()) {
+			return;
+		}
+
+		if (form.Class == null) {
+			MessageBox.Show(
+				"ClassEditForm вернула null",
+				"Внутренняя ошибка"
+			);
+			return;
+		}
+		if (!form.Class.IsValid) {
+			MessageBox.Show("Неправильно указаны данные!");
+			return;
+		}
+
+		this._class = form.Class;
+	}
+
+	/// <summary> Обработчик создания нового занятия </summary>
+	private void ClassAddStripMenuItem_Click (object sender, EventArgs e)
+		=> this.AddOrEditClass(new Class());
+
+	/// <summary> Обработчик редактирования существующего занятия </summary>
+	private void ClassEditStripMenuItem_Click (object sender, EventArgs e) 
+		=> this.AddOrEditClass(_class.Clone());
 }
