25ee936d60b5bade8acea5616d48c48199aca12f
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-11-02 10:29:51 +0300

    Добавил в оценки массив с возможными значениями

--- Electives/Mark.cs
+++ Electives/Mark.cs
@@ -1,9 +1,11 @@
-﻿namespace Electives;
+﻿using System.Threading;
+
+namespace Electives;
 
 /// <summary>Оценка</summary>
 /// <param name="t">Значение</param>
 public class Mark (Mark.Type t = Mark.Type.None)
-{	
+{
 	/// <summary>Возможное значение оценки</summary>
 	public enum Type : byte
 	{
@@ -23,14 +25,25 @@ public class Mark (Mark.Type t = Mark.Type.None)
 		Extra
 	}
 
+	/// <summary> Массив со всеми возможными значениями типов </summary>
+	static public Mark[] Types { get; } = [
+		new Mark(Type.None),
+		new Mark(Type.Skip),
+		new Mark(Type.Two),
+		new Mark(Type.Three),
+		new Mark(Type.Four),
+		new Mark(Type.Five),
+		new Mark(Type.Extra)
+	];
+
 	/// <summary> Свойство со значением оценки </summary>
 	public Type Value { get; set; } = t;
 
-	public override string ToString()
+	public override string ToString ()
 	{
 		return Value switch
 		{
-			Type.None => " ",
+			Type.None => "-",
 			Type.Skip => "н",
 			Type.Two => "2",
 			Type.Three => "3",
