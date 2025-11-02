9440c8236d367f20887fee8eec5e575308c302fd
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-11-02 13:26:48 +0300

    XML-коментарии формы редактирования плана

--- Interface/FormPlan.cs
+++ Interface/FormPlan.cs
@@ -2,17 +2,29 @@
 
 namespace Interface;
 
+/// <summary> Форма редактирования плана </summary>
 public partial class FormPlan : Form
 {
+	/// <summary> Поле с редактируемым планом </summary>
 	public Electives.Plan Plan { get; init; }
+
+	/// <summary> Конструктор </summary>
+	/// <param name="plan">Редактируемый план</param>
 	public FormPlan (Electives.Plan plan)
 	{
 		this.Plan = plan;
 		this.InitializeComponent();
 	}
 
+	/// <summary> Обработчик нажатия кнопки "Закрыть" </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
 	private void buttonClose_Click (object sender, EventArgs e) => this.Close();
 
+
+	/// <summary> Обработчик нажатия кнопки "ОК" </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
 	private void buttonOK_Click (object sender, EventArgs e)
 	{
 		var selectedStudent = this.comboBoxStudents.SelectedItem as Electives.Student;
@@ -32,6 +44,9 @@ public partial class FormPlan : Form
 		this.Close();
 	}
 
+	/// <summary> Наполнение и установка элементов формы </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
 	private void FormPlan_SetBoxes (object sender, EventArgs e)
 	{
 		foreach (var kvPair in Journal.ListClasses) { 
@@ -52,7 +67,6 @@ public partial class FormPlan : Form
 			this.comboBoxMarks.Items.Add(posMark);
 		}
 
-		this.comboBoxMarks.SelectedIndex = (int)this.Plan.Mark.Value;
-		
+		this.comboBoxMarks.SelectedIndex = (int)this.Plan.Mark.Value;		
 	}
 }
